8d1d63da91271b1de2a71164eae96f324be4d977
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-05-07 17:19:16 +0530

    crud

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,23 @@
 const Task =require('../models/Tasks')
 
-const getAlltasks = (req,res)=>{
-    res.send('all tasks')
+const getAlltasks = async (req,res) => {
+    try {
+        const task = await Task.find({})
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 const createTask = async (req,res)=>{
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
-    console.log(task)
+    try{
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+        console.log(task)
+
+    }catch(error){
+        res.status(500).json({msg:error})
+        console.log(error)
+    }
 }
 const getSingleTask = (req,res)=>{
     res.json({id:req.params.id})
