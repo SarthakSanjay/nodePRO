90b1b7170ade0e875a8d35ec3c7f9fb82d1044f1
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-05-12 22:42:13 +0530

    commited changes

--- 2_store/controllers/product.js
+++ 2_store/controllers/product.js
@@ -1,12 +1,17 @@
-
-const getALlProductsStatic = async(req,res) =>{
-    throw new Error('testing async errors')
-    res.status(200).json({message:"product testing route"})
+const product = require('../models/product')
+const Products = require('../models/product')
+const getALlProductsStatic = async (req, res) => {
+    // throw new Error('testing async errors')
+    const product = await Products.find({
+        featured: true
+    })
+    res.status(200).json({ product ,
+        nbHits: product.length})
 }
-const getALlProducts = async(req,res) =>{
-    res.status(200).json({message:"product  route"})
+const getALlProducts = async (req, res) => {
+    res.status(200).json({ message: "product  route", product: Products })
 }
 
-module.exports ={
-    getALlProducts,getALlProductsStatic
+module.exports = {
+    getALlProducts, getALlProductsStatic
 }
--- 2_store/models/product.js
+++ 2_store/models/product.js
@@ -33,4 +33,4 @@ const productSchema = mongoose.Schema({
 
 })
 
-module.exports = mongoose.model['Product', productSchema]
+module.exports = mongoose.model('Products', productSchema)
