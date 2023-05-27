2a1bd1bed0dcdcbef8a8919346aa713e4f15c907
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-05-28 00:43:42 +0530

    add more func

--- mongoose/blogApp.js
+++ mongoose/blogApp.js
@@ -6,49 +6,70 @@ const readline = require('readline')
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
-  const createBlog = () =>{
- rl.question('Enter the title: ', (title) => {
-            rl.question('Enter the content: ', (content) => {
-              rl.question('Enter the author: ', (author) => {
-                 BLOG.create({
-                    title:title,
-                    content:content,
-                    author:author,
-        
-                }).then(()=>console.log("blog published"))
-                .catch((error)=> {
-                    console.log(error)
-                })
-                })
-                })
-                })
-                
-  }
-  
-  const seeAllBlogs = async() =>{
-    await BLOG.find({})
-    .then((d)=>console.log(d))
-    .catch((error)=> {
-        console.log(error)
+const createBlog = () => {
+    rl.question('Enter the title: ', (title) => {
+        rl.question('Enter the content: ', (content) => {
+            rl.question('Enter the author: ', (author) => {
+                BLOG.create({
+                    title: title,
+                    content: content,
+                    author: author,
+
+                }).then(() => console.log("blog published"))
+                    .catch((error) => {
+                        console.log(error)
+                    })
+                    .finally(() => {
+                        rl.close()
+                    })
+            })
+        })
     })
-  }
 
-  const totalDocs = async()=>{
-    await BLOG.countDocuments().then(count => console.log(count)).catch((e) => console.log(e))
-    
 }
-const run = async() => {
-    try {
-        await mongoose.connect(process.env.BLOG)
-        .then(console.log('connected to db'))
-        // .then(createBlog() ,console.log('inserted blog'))
-        .then(seeAllBlogs(),totalDocs())
-        .catch((error)=> {
+
+const seeAllBlogs = async () => {
+    await BLOG.find({})
+        .then((d) => console.log(d))
+        .catch((error) => {
             console.log(error)
         })
-       
+}
+
+const updateBlog = (id, title , author) =>{
+    BLOG.updateOne({'_id':id},{title: title , author : author})
+    .then(()=>{
+        console.log("blog title updated")
+    }).catch((e)=>console.log(e))
+}
+const deleteABlog = (id) =>{
+    BLOG.deleteOne({'_id': id})
+    .then(()=>console.log('blog deleted'))
+    .catch((e)=>{console.log(e)})
+}
+const deleteAllBlog = () =>{
+    BLOG.deleteMany()
+}
+const totalDocs = async () => {
+    await BLOG.countDocuments().then(count => console.log(count)).catch((e) => console.log(e))
+
+}
+const run = async () => {
+    try {
+        await mongoose.connect(process.env.BLOG)
+            .then(console.log('connected to db'))
+            // createBlog()
+            // updateBlog("64724e9bf0ddcd488e3e4ef8","one piece anime", "luffy chacha")
+            // deleteABlog("64724e9bf0ddcd488e3e4ef8")
+            seeAllBlogs()
+            totalDocs()
+            .catch((error) => {
+                console.log(error)
+            })
+          
+
 
         // await BLOG.create({
         //     title:"Will AI replace jobs?",
@@ -60,7 +81,7 @@ const run = async() => {
         //     console.log(error)
         // })
 
-        
+
 
     } catch (error) {
         console.log(error)
