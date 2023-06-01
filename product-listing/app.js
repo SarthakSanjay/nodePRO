00bc4480e31f641e7bdcb0569fdd90cbfd946f27
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-06-01 23:28:55 +0530

    ic

--- product-listing/app.js
+++ product-listing/app.js
@@ -2,14 +2,22 @@ require("dotenv")
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
-
-const start = () =>{
-    try {
-        app.listen(port,()=>{
-            console.log("server this 3000")
-        })  
-    } catch (error) {
-        console.log(error)
-    }
-}
-start()
+app.use(express.urlencoded({extended:true}))
+app.set('view engine','ejs')
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+app.post('/login',(req,res)=>{
+    console.log(req.body)
+})
+app.listen(port,()=>{
+    console.log("server this 3000")
+})  
+// const start = () =>{
+//     try {
+       
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// start()
