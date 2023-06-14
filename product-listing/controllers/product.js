11f10ecdfec48c57f82644e7ae2f8bee11de3c4b
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-06-14 21:41:07 +0530

    added delete product route

--- product-listing/controllers/product.js
+++ product-listing/controllers/product.js
@@ -1,8 +1,10 @@
 const PRODUCT = require('../models/product')
+
 const getAllProducts = async(req,res)=>{
     const products = await PRODUCT.find({})
     res.status(200).json({total:products.length,products:products})
 }
+
 const getProduct = async (req,res) =>{
     // const name = req.body.name
     const {name} = req.query
@@ -15,4 +17,17 @@ const getProduct = async (req,res) =>{
     }
     res.status(200).json({ product:product})
 }
-module.exports = {getAllProducts , getProduct}
+
+const deleteProduct = async (req,res) =>{
+    // const name = req.body.name
+    const {name} = req.query
+    console.log(req.query)
+    const product = await PRODUCT.deleteOne({productName:String(name)})
+    console.log(name)
+    console.log(product)
+    if(!product){
+     return res.status(404).send("Not found")
+    }
+    res.status(200).json({ product:product})
+}
+module.exports = {getAllProducts , getProduct , deleteProduct}
--- product-listing/routes/product.js
+++ product-listing/routes/product.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {getAllProducts,getProduct} = require('../controllers/product')
+const {getAllProducts,getProduct ,deleteProduct} = require('../controllers/product')
 
 router.route('/').get(getAllProducts)
-router.route('/product').get(getProduct).post(getProduct)
+router.route('/product').get(getProduct).post(getProduct).delete(deleteProduct)
 module.exports = router
 
 
